d84bcb5bd48f2189d81293d1d296c69f29d0b58f
absakash <akashboswas2@gmail.com>
2024-02-16 19:00:34 +0530

    first added

---
+++ src/shared/Fotter.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Fotter = () => {
+  return (
+    <div className="mt-96">
+      <button className="btn btn-outline btn-info">Info</button>
+      <button className="btn btn-outline btn-success">Success</button>
+      <button className="btn btn-outline btn-warning">Warning</button>
+      <button className="btn btn-outline btn-error">Error</button>
+    </div>
+  );
+};
+
+export default Fotter;
